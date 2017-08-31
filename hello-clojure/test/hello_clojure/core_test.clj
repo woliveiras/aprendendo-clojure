1aaf1d0cee74f825d53e06af45739d4703bc1e2d
woliveiras <w.oliveira542@gmail.com>
2017-08-31 20:49:47 -0300

    starting project with lein new project

---
+++ hello-clojure/test/hello_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns hello-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [hello-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
